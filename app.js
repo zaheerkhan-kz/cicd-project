5c9094168ce40432a0e5983a5bf2c7bb354946ad
zaheerkhan-kz <zaheerkhan.nagarwala@gmail.com>
2025-12-11 17:57:56 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from CI/CD example project!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
